e6e6df7bfd5adf7b0f81f757fd02a35912e58713
David Crumpton <crumptondavid@gmail.com>
2017-06-18 17:34:26 -0600

    Initial Commit

---
+++ mainly/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mainly
+//
+//  Created by David Crumpton on 18/Jun/17.
+//  Copyright © 2017 David Crumpton. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
